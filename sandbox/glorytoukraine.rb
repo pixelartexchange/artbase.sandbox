c9cd8789ecaa8e3bd87dc54e5414027c9f9c6028
Gerald Bauer <gerald.bauer@gmail.com>
2022-04-29 16:53:36 +0200

    up

--- sandbox/glorytoukraine.rb
+++ sandbox/glorytoukraine.rb
@@ -14,21 +14,46 @@
 require 'pixelart'
 
 
-###
-#  try the first nine moonbirds 42x42
-birds = ImageComposite.new( 3, 3, width: 42, height: 42)
+##
+#  try top selling golden moonbirds 42x42
+#    see  https://www.nft-stats.com/collection/proof-moonbirds
 
-ids = [0,1,2,
-       3,4,5,
-       6,8,9]
+composite = ImageComposite.new( 6, 3, width: 42, height: 42)
 
-ids.each do |id|
-  bird = Image.read( "./proof-moonbirds/42x42/#{id}.png" )
-  birds << bird.transparent.ukraine
+birds = [5602,7789,8249,
+         6610,7205,2018,
+         761,7473,3047].map { |id| Image.read( "./proof-moonbirds/42x42/#{id}.png" ) }
+
+
+birds.each_slice(3) do |row|
+  (0..1).each do |series|
+    row.each do |bird|
+      bird = bird.transparent.ukraine  if series == 1
+      composite << bird
+    end
+  end
 end
 
 
-birds.save( "./tmp/moonbirds-ukraine.png" )
-birds.zoom(4).save( "./tmp/moonbirds-ukraine@4x.png" )
+composite.save( "./tmp/moonbirds-ukraine-golden.png" )
+composite.zoom(4).save( "./tmp/moonbirds-ukraine-golden@4x.png" )
+
+
+
+###
+#  try the first nine moonbirds 42x42
+composite = ImageComposite.new( 3, 3, width: 42, height: 42)
+
+birds = [0,1,2,
+         3,4,5,
+         6,8,9].map {|id| Image.read( "./proof-moonbirds/42x42/#{id}.png" ) }
+
+birds.each do |bird|
+  composite << bird.transparent.ukraine
+end
+
+
+composite.save( "./tmp/moonbirds-ukraine.png" )
+composite.zoom(4).save( "./tmp/moonbirds-ukraine@4x.png" )
 
 puts "bye"
