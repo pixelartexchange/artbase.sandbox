233989fe3c11adfd9a7a2d295a4ca9655eba5011
Gerald Bauer <gerald.bauer@gmail.com>
2022-06-26 16:49:34 +0200

    up

---
+++ babymoonrunners/collection.rb
@@ -0,0 +1,11 @@
+##
+# see   https://opensea.io/collection/babymoonrunners
+
+
+COLLECTION = TokenCollection.new( 'babymoonrunners', 3333,
+                     token_base: 'ipfs://QmbnSzLxLcpue9JUjBFAgEKXX8RsodhJGSfFCnarBfMovi/{id}',
+                     image_base: 'ipfs://bafybeihqlcfwnmuwrejq4li6mj4zxhhznxwvc2aox4zlg47kx2lozlry74/{id}.png',
+                     format: '100x100',
+                     source: '512x512',
+                     offset: 1
+)
