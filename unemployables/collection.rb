2d56f6a3bfa28a32db61ec11a7dcf26a4e9aeadf
Gerald Bauer <gerald.bauer@gmail.com>
2022-07-06 19:57:05 +0200

    up

--- unemployables/collection.rb
+++ unemployables/collection.rb
@@ -8,22 +8,16 @@
 
 ##
 ##  todo - check if 5000 items or 5001 (-- why 5001 on open sea listed?)
+##        a:  includes dummy no. 0 (see below)
 
-COLLECTION = TokenCollection.new( 'unemployables', 5001,
+## yes,  no.0  is a dummy (forever "unrevealed")
+##   see https://opensea.io/assets/ethereum/0xe0be388ab81c47b0f098d2030a1c9ef190691a8a/0
+
+## note: #0  return 404 not found!!! - skip officially - why? why not?
+
+
+COLLECTION = TokenCollection.new( 'unemployables', 5000,
                           token_base: 'ipfs://QmZxDanojLvjgzhMe1gBaNdMygqh743xYH4uZpcZVshxUc/{id}',
                           format: '24x24',
                           source: '600x600',
                           offset: 1 )
-
-
-## note: #0  return 404 not found!!! - skip officially - why? why not?
-# range = (1000..1999)
-# c.download_meta( range )
-
-
-# range = (179..361)
-# c.download_images( range )
-
-
-# range = (1..361)
-# c.pixelate( range )
